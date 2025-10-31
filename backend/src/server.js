59464b6bba191187b8c8f5f8ce3b19f11961a802
dauvannghia <nghiadauvan@gmail.com>
2025-10-31 11:13:51 +0700

    ready for deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,21 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRouter from "./routes/auth.route.js";
-
+import path from "path";
 dotenv.config();
 
 const app = express();
+const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/v1/auth", authRouter);
+
+//make ready for deloyment
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+  app.get("*", (_, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 app.listen(3000, () => console.log("Server is running on post" + PORT));
