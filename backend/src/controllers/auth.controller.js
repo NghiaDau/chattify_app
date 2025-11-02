4efe20b44ec61774b5dfc10e72d504671320b1b2
dauvannghia <nghiadauvan@gmail.com>
2025-11-02 21:39:15 +0700

    add funtion login, logout

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -42,3 +42,31 @@ export const signup = async (req, res) => {
     return res.status(500).json({ message: "Server error" });
   }
 };
+
+export const login = async (req, res) => {
+  const { email, password } = req.body;
+  try {
+    const user = await User.findOne({ email });
+    if (!user) return res.status(400).json({ message: "User is not existed" });
+
+    const checkPass = await bcrypt.compare(password, user.password);
+    if (!checkPass) return res.status(400).json({ message: "Wrong password" });
+
+    generateToken(user._id, res);
+
+    return res.status(200).json({
+      _id: user._id,
+      fullName: user.fullName,
+      email: user.email,
+      profilePic: user.profilePic,
+    });
+  } catch (error) {
+    console.error("Error in controler");
+    return res.status(500).json({ message: "Internal Server" });
+  }
+};
+
+export const logout = async (_, res) => {
+  res.cookie("jwt", "", { maxAge: 0 });
+  res.status(200).json({ message: "logout succesfully" });
+};
