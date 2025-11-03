62428dc46fbcfbe80693058cb5d672c5459ee686
dauvannghia <nghiadauvan@gmail.com>
2025-11-03 13:51:05 +0700

    add chat

--- backend/src/server.js
+++ backend/src/server.js
@@ -4,13 +4,17 @@ import authRouter from "./routes/auth.route.js";
 import messageRouter from "./routes/message.route.js";
 import path from "path";
 import { connectDB } from "./lib/db.js";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 dotenv.config();
+import "dotenv/config";
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = process.env.PORT || 3000;
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+app.use(cors({ origin: process.env.CLIENT_URL, credentials: true }));
 app.use(cookieParser());
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/message", messageRouter);
--- backend/src/utils/jwt.util.js
+++ backend/src/utils/jwt.util.js
@@ -7,8 +7,9 @@ export const generateToken = (userId, res) => {
   res.cookie("jwt", token, {
     maxAge: 7 * 24 * 60 * 60 * 1000,
     httpOnly: true,
-    sameSite: "strict",
-    secure: process.env.NODE_ENV === "development" ? false : true,
+    sameSite: "none",
+    //secure: process.env.NODE_ENV === "development" ? false : true,
+    secure: true,
   });
   return token;
 };
